3f3c0ac0116ac85eb5f3e8f76f51815fa44f8d7c
Touqeer Shahid Abbasi <73354832+TouqeerShahid951@users.noreply.github.com>
2024-11-04 20:28:25 +0500

    Time: 3 ms (54.20%), Space: 17.9 MB (10.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,8 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}
-        for i, num in enumerate(nums):
-            b = target-num
-            if b in num_dict:
-                return [num_dict[b],i]
-            num_dict[num]=i
-       
-        
+class Solution: 
+    def twoSum(self, nums: List[int], target: int) -> List[int]: 
+        ndict = {} 
+        for i,num in enumerate(nums): 
+            b = target-num 
+            if b in ndict: 
+                return i, ndict[b] 
+            ndict[num]=i
